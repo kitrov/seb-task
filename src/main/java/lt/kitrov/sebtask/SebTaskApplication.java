bace5caeb27030fccc2e1d2e8d5bec7957a80306
Viktor Juša <v.jusa@adnera.lt>
2018-08-24 22:36:23 +0300

    init commit

---
+++ src/main/java/lt/kitrov/sebtask/SebTaskApplication.java
@@ -0,0 +1,12 @@
+package lt.kitrov.sebtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SebTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SebTaskApplication.class, args);
+    }
+}
